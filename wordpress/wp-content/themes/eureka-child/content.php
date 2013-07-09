f9a114faf5ff04eac35cfd90660f961af18fc70a
portmantina <ccauterucci@gmail.com>
2013-07-09 19:54:31 -0400

    Taking dates out of posts

--- wordpress/wp-content/themes/eureka-child/content.php
+++ wordpress/wp-content/themes/eureka-child/content.php
@@ -12,7 +12,7 @@
 	
 	<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
-			<?php eureka_posted_on(); ?>
+			<!--<?php eureka_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
